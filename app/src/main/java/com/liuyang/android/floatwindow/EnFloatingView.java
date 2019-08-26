f8f88830cc2f914bb87f42d4175b307bc1948789
liuyang <981872372@qq.com>
2019-08-26 16:20:08 +0800

    Initial commit

---
+++ app/src/main/java/com/liuyang/android/floatwindow/EnFloatingView.java
@@ -0,0 +1,30 @@
+package com.liuyang.android.floatwindow;
+
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
+/**
+ * @ClassName EnFloatingView
+ * @Description 悬浮窗
+ * @Author Yunpeng Li
+ * @Creation 2018/3/15 下午5:04
+ * @Mender Yunpeng Li
+ * @Modification 2018/3/15 下午5:04
+ */
+public class EnFloatingView extends FloatingMagnetView {
+
+    private final ImageView mIcon;
+
+    public EnFloatingView(@NonNull Context context) {
+        super(context, null);
+        inflate(context, R.layout.en_floating_view, this);
+        mIcon = findViewById(R.id.icon);
+    }
+
+    public void setIconImage(@DrawableRes int resId){
+        mIcon.setImageResource(resId);
+    }
+
+}
